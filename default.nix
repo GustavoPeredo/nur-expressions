003f93bd295c3c6961c91628695c11f8edf17cd9
GustavoPeredo <gustavomperedo@protonmail.com>
2023-02-03 01:58:59 +0100

    Add flatpak hm-module

--- default.nix
+++ default.nix
@@ -11,10 +11,9 @@
 {
   # The `lib`, `modules`, and `overlay` names are special
   lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
+  hm-modules = import ./hm-modules; # home-manager modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
